7e0420043e9d91c2827988cf69b47f86464768c3
Lance <slicer229@gmail.com>
2023-12-18 22:42:16 -0700

    fixed index.js

--- sait-express 2023/index.js
+++ sait-express 2023/index.js
@@ -1,22 +1,59 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const greeting = require("./Public/greeting");
+const greeting = require("../sait-express pug files/Public/greeting");
+var mysql = require('mysql')
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "Jennifer",
+//password: "jennifer58312566",
+database: "travelexperts"
+});
+
+const app = express();
+
+con.connect(function(err) {
+if (err) throw err;
+console.log("Connected!");
+});
+
+var sql = ("select * FROM customers");
+con.query(sql, function (err, result) {
+if (err) throw err;
+console.log("Result: " + result);
+});
+
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname,"./views"),{extensions:["html","htm",]}));
+app.use(express.static(path.join(__dirname,"./public"),{extensions:["css","js",]}));
+app.use(express.static(path.join(__dirname,"./media"),{extensions:["gif","jpeg","jpg", "png"]}));
+
+
+
 app.listen(8000, (err)=>{
     if (err) throw err;
     console.log("server started on port 8000");
 });
-/*app.get("/about",(req, res)=>{
-    res.send(greeting.greet());
-});*/
-app.use(express.static(path.join(__dirname,"views"),{extensions:["html","htm",]}));
-app.use(express.static(path.join(__dirname,"public"),{extensions:["css","js",]}));
-app.use(express.static(path.join(__dirname,"media"),{extensions:["gif","jpeg","jpg", "png"]}));
 app.use(express.urlencoded({extended: true}));
-app.get("/", (req, res) => {
-    res.render("index");
-    });
-    
-app.use((req, res, next)=>{
-    res.status(404).send("Sorry, can't find that.");
+
+app.get("/",(req, res)=>{
+    res.render("index",{greeting: greeting.greet()});
 });
+app.get("/contact2",(req, res)=>{
+    res.render("contact2");
+});
+app.get("/about",(req, res)=>{
+    res.render("about");
+});
+app.get("/404",(req, res)=>{
+    res.render("404");
+});
+app.get("/register",(req, res)=>{
+    res.render("register");
+});
+app.use((req, res, next)=>{
+    res.status(404).render("404");
+    next();
+});
+
+
